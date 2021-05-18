3dd5cf387eae180cd1797da7319a68f40a4b2375
EC2 Default User <ec2-user@ip-172-31-80-129.ec2.internal>
2021-05-18 01:01:10 +0000

    added new file

--- day_2/security_group.tf
+++ day_2/security_group.tf
@@ -30,6 +30,45 @@ resource "aws_security_group" "cloud-test" {
         cidr_blocks = [var.cidr_blocks]
     }
 
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = [var.cidr_blocks]
+    }
+
+
+     ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = [var.cidr_blocks]
+    }
+     ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = [var.cidr_blocks]
+    }
+     ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = [var.cidr_blocks]
+    } 
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = [var.cidr_blocks]
+    }
+      ingress {
+        from_port = 5000
+        to_port = 5000
+        protocol = "tcp"
+        cidr_blocks = [var.cidr_blocks]
+    }
+
     egress {
         from_port = 8080
         to_port = 8080
